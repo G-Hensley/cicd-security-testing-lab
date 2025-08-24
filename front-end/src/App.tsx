e4668429956d109f08b9f438a33f80732059f6f2
Gavin Hensley <gavinhensley@protonmail.com>
2025-08-24 14:53:42 -0400

    Add some basic content

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,10 @@
 export default function App() {
-  return <div className="text-3xl font-bold underline">Hello world!</div>
+  return (
+    <main className="h-screen w-screen p-8 flex flex-col items-center bg-zinc-900 text-white">
+      <header className="flex flex-col items-center gap-2">
+        <h1 className="text-5xl font-serif">CI/CD Testing</h1>
+        <h2 className="text-2xl w-md text-center">A playground for testing components, app tests, and security testing</h2>
+      </header>
+    </main>
+  )
 }
